973cf93558410b08b5d636ed37f7ba9af0109864
Mahesh-Andey <97396160+Mahesh-Andey@users.noreply.github.com>
2022-08-09 20:47:41 +0530

    first commit

--- mlapi/settings.py
+++ mlapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vg60x___e+m8iu0%wynoo8q3kz$(&!z#e5+^3fb**1$z+m$bjd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
