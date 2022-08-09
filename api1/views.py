72d2d1b0951d84efe513b89b30ee2fa935594380
Mahesh-Andey <97396160+Mahesh-Andey@users.noreply.github.com>
2022-08-09 19:42:09 +0530

    first commit

---
+++ api1/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view
+
+import numpy as np
+import cv2 as cv
+from PIL import Image
+import io
+from base64 import b64decode, b64encode
+
+
+
+
+
+# Create your views here.
+
+@api_view(['POST'])
+def ORB(request):
+        # RefImg = request.data.image1
+        # QueryImg = request.data.image2
+
+
+        # data = JSONParser().parse(request)
+        # serializer = ImageSerializer(data)
+        # new_data = serializer.data['im1']+serializer.data['im2']
+
+
+        base64_data1 = request.data.get("im1", None).split(',', 1)[1]
+        base64_data2 = request.data.get("im2", None).split(',', 1)[1]
+        data = b64decode(base64_data1)
+        data2 = b64decode(base64_data2)
+        pimg = Image.open(io.BytesIO(data))
+        pimg2 = Image.open(io.BytesIO(data2))
+
+
+        training_image = cv.cvtColor(np.array(pimg), cv.COLOR_BGR2RGB)
+        test_image = cv.cvtColor(np.array(pimg2), cv.COLOR_BGR2RGB)
+
+
+        training_gray = cv.cvtColor(training_image, cv.COLOR_RGB2GRAY)
+        test_gray = cv.cvtColor(test_image, cv.COLOR_RGB2GRAY)
+
+        MIN_MATCH_COUNT = 10
+
+        orb = cv.ORB_create()
+
+        kp1, des1 = orb.detectAndCompute(training_image,None)
+        kp2, des2 =  orb.detectAndCompute(test_image,None)
+        FLANN_INDEX_KDTREE = 1
+
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+
+        matches=flann.knnMatch(np.asarray(des1,np.float32),np.asarray(des2,np.float32), 2)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+        if len(good)>MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+            h,w = training_gray.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv.perspectiveTransform(pts,M)
+            img2 = cv.polylines(test_image,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        else:
+            print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+            matchesMask = None
+        
+        imm = test_image.copy()
+
+        matrix = cv.getPerspectiveTransform(dst, pts)
+
+        destination_image = cv.warpPerspective(imm, matrix, (1600, 1600))
+        
+        img = Image.fromarray(destination_image, 'RGB')
+        si = img.resize((300,300),Image.ANTIALIAS)
+        buffered = io.BytesIO()
+        si.save(buffered, format="JPEG")
+        img_str = b64encode(buffered.getvalue())
+
+        final_output = {
+            "Transformed Image": img_str
+        }
+
+        return Response(final_output)
+
+
+
+
