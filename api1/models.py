058b28f5971bb5a2ddeb001e1dc84e7415bf6354
Mahesh-Andey <97396160+Mahesh-Andey@users.noreply.github.com>
2022-08-27 12:11:59 +0530

    maksmd

--- api1/models.py
+++ api1/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# class SIFTImageModel(models.Model):
-#     image1 = models
+class SIFTImageModel(models.Model):
+    image1 = models.ImageField()
 
 
--- api1/urls.py
+++ api1/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     # path("orb/", views.ORB),
-    path("sift/add-test-img/", views.SIFT),
-    path("sift/add-ref-img/", views.addRefImg),
-    # path("sift/", views.SIFT)
+    # path("sift/add-test-img/", views.SIFT),
+    # path("sift/add-ref-img/", views.addRefImg),
+    path("sift/", views.SIFT)
 ]
 
 
